a6408befc449dee71858935e75038c9ec95c5405
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-03-09 01:40:51 +0900

    added revise_settings_json()

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 
@@ -17,3 +18,10 @@ def revise_bashrc():
 
 def get_settings_json_filename():
     return os.path.expandvars(os.path.join('$APPDATA', 'Code', 'User', 'settings.json'))
+
+
+def revise_settings_json(json_filename=get_settings_json_filename()):
+    with open(json_filename, 'r') as json_file:
+        settings = json.load(json_file)
+
+    print(settings)
