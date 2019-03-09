c5297c43bd73ea06e17a092ce0c7f4bb3d06e9ae
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-03-09 10:52:11 +0900

    added test_get_conda_sh_filename()

--- test_config.py
+++ test_config.py
@@ -1,6 +1,18 @@
 import config
+import os
 
 
 def test_does_this_file_exist():
     # Following should not raise an exception
     config.does_file_exist(__file__)
+
+
+def test_get_conda_sh_filename():
+    result = config.get_conda_sh_filename()
+    assert isinstance(result, str)
+
+    result_list = result.split(os.sep)
+    assert 'conda.sh' == result_list[-1]
+    assert 'Anaconda3' in result_list
+    assert 'etc' in result_list
+    assert 'profile.d' in result_list
