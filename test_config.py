c65414119c5ce7935c3e87dcf2b3b5c72f896946
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-03-16 14:36:05 +0900

    added test_get_re_export_path_sample()

--- test_config.py
+++ test_config.py
@@ -133,6 +133,17 @@ def test_add_python_folder_to_path():
     assert config.get_unix_path(python_folder) in result[1], result
 
 
+def test_add_python_folder_to_path_example():
+    input_text = '''export LANG=en_US.utf8
+export PATH=~/Anaconda3:$PATH
+. ~/Anaconda3/etc/profile.d/conda.sh
+alias log="git log --oneline --graph --all --decorate"
+'''
+    python_folder = os.path.split(sys.executable)[0]
+    result = config.add_python_folder_to_path(input_text, bash_can_find_python=False, conda_folder=python_folder)
+    assert config.get_unix_path(python_folder) in result[1], result
+
+
 def test_add_python_folder_to_path_default():
     bashrc_text = config.read_file(os.path.expanduser('~/.bashrc'))
     b_update, result = config.add_python_folder_to_path(bashrc_text)
@@ -180,27 +191,12 @@ def test_revise_settings_json():
     config.revise_settings_json(b_save=False)
 
 
-def test_get_unix_path_with_drive():
-    input_win_path = 'C:\\Users\\beachgoer\\.bashrc'
-    result = config.get_unix_path(input_win_path)
-    expected = '/c/Users/beachgoer/.bashrc'
-
-    assert expected == result, f"\nexpected : {expected}\nresult : {result}"
-
-
 def test_get_unix_path_without_drive():
-    input_win_path = 'Users\\beachgoer\\.bashrc'
+    input_win_path = 'Users\\user\\.bashrc'
     result = config.get_unix_path(input_win_path)
-    expected = 'Users/beachgoer/.bashrc'
+    expected = 'Users/user/.bashrc'
 
     assert expected == result, f"\nexpected : {expected}\nresult : {result}"
-
-
-def test_run_cmd_in_bash():
-    result = config.run_cmd_in_bash('true && echo $?')
-    expected = '0\n'
-
-    assert result == expected, result
 
 
 def test_which_python_unix_path():
@@ -217,6 +213,21 @@ def test_which_python_unix_path():
 
     assert os.path.exists(result_win_path), (sys.executable, result_win_path)
     assert os.path.isfile(result_win_path), (sys.executable, result_win_path)
+
+
+def test_run_cmd_in_bash():
+    result = config.run_cmd_in_bash('true && echo $?')
+    expected = '0\n'
+
+    assert result == expected, result
+
+
+def test_get_unix_path_with_drive():
+    input_win_path = 'C:\\Users\\user\\.bashrc'
+    result = config.get_unix_path(input_win_path)
+    expected = '/c/Users/user/.bashrc'
+
+    assert expected == result, f"\nexpected : {expected}\nresult : {result}"
 
 
 def test_add_alias_line():
@@ -251,6 +262,26 @@ def test_get_re_export_path():
     assert expected in result, (r, result, input_text)
     assert expected_1 in result, (r, result, input_text)
     assert not_expected not in result, (r, result, input_text)
+
+
+def test_get_re_export_path_sample():
+    expected = '~/Anaconda3:$PATH'
+    expected_1 = '~/Anaconda3:/~/Anaconda3/Library/bin:$PATH'
+
+    r = config.get_re_export_path()
+
+    input_text = f'''export LANG=en_US.utf8
+export PATH={expected}
+. ~/Anaconda3/etc/profile.d/conda.sh
+alias log="git log --oneline --graph --all --decorate"
+
+export PATH={expected_1}
+'''
+
+    result = r.findall(input_text)
+
+    assert expected in result, (r, result, input_text)
+    assert expected_1 in result, (r, result, input_text)
 
 
 def test_add_to_list_unique_at_0():
