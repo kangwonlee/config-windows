79f65343f4b7d98a8bea5cc91f6307e77ceefb78
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-05-26 16:34:23 +0900

    gitflow.prefix

--- run_this.sh
+++ run_this.sh
@@ -2,3 +2,9 @@
 source ~/.bashrc
 which python
 python -c "import numpy"
+
+git config --global gitflow.prefix.feature "feature/"
+git config --global gitflow.prefix.bugfix "bugfix/"
+git config --global gitflow.prefix.release "release/"
+git config --global gitflow.prefix.hotfix "hotfix/"
+git config --global gitflow.prefix.support "support/"
