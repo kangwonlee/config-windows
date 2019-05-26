7b43271e971f3b5075beb6635b4948530b6e6b31
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-05-26 20:02:47 +0900

    refactored gitflow.prefix configuration

---
+++ config_git_flow.sh
@@ -0,0 +1,5 @@
+git config --global gitflow.prefix.feature "feature/"
+git config --global gitflow.prefix.bugfix "bugfix/"
+git config --global gitflow.prefix.release "release/"
+git config --global gitflow.prefix.hotfix "hotfix/"
+git config --global gitflow.prefix.support "support/"
--- run_this.sh
+++ run_this.sh
@@ -2,9 +2,3 @@
 source ~/.bashrc
 which python
 python -c "import numpy"
-
-git config --global gitflow.prefix.feature "feature/"
-git config --global gitflow.prefix.bugfix "bugfix/"
-git config --global gitflow.prefix.release "release/"
-git config --global gitflow.prefix.hotfix "hotfix/"
-git config --global gitflow.prefix.support "support/"
